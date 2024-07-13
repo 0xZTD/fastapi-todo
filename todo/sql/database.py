0dfb29544a5fc6cda247c4c8f755cb26bfb0f76c
zestx5 <buyankov.ilya@gmail.com>
2024-07-13 10:26:30 +0300

    add: docker-compose

--- todo/sql/database.py
+++ todo/sql/database.py
@@ -1,11 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLACLHEMY_DATABASE_URL = "sqlite:///./todo.db"
+root_path = os.path.dirname(os.path.abspath(__file__))
+SQLITE_FALLBACK = f"sqlite:///{root_path}/todo.db"
+SQLACLHEMY_DATABASE_URL = os.getenv("DATABASE_URL") or SQLITE_FALLBACK
 
-engine = create_engine(
-    SQLACLHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLACLHEMY_DATABASE_URL)
+if SQLACLHEMY_DATABASE_URL == SQLITE_FALLBACK:
+    engine = create_engine(
+        SQLACLHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
